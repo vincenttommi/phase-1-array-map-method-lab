ec37d112a6480488f89108185fe033c01ff03d9a
vincenttommi <vincenttommi@gmail.com>
2023-03-21 04:55:29 +0300

    completed

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// const titleCased = () => {
+//   return tutorials
+// }
+
+function titleCased() {
+
+
+  //This function uses the map method of the Array object to create a new 
+  //array by iterating through each element in the tutorials array.
+  // For each tutorial, it splits the string into an array of words using the 
+  //split method and then uses the map method again to iterate through each word in the array. For each word, it capitalizes the first letter using the charAt and toUpperCase methods and then appends the rest of the word using the slice method. Finally, it joins the words back into a string using the join method and returns the title-cased tutorial name.
+  return tutorials.map(function(tutorial) {
+    return tutorial.split(' ').map(function(word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  });
 }
